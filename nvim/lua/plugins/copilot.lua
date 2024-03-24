5444422a1acbb5bf827f5f83af4e628fd99a7966
Peter Panduro <email@peterpanduro.com>
2024-03-24 14:52:31 +0100

    Improved Copilot keybinds

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,5 +1,13 @@
 return {
     "github/copilot.vim",
     event = "VeryLazy",
-    cmd = "Copilot"
+    cmd = "Copilot",
+    config = function()
+        vim.keymap.set("i", "<C-Y>", 'copilot#Accept("\\<CR>")', {
+            expr = true,
+            replace_keycodes = false,
+        })
+        vim.g.copilot_no_tab_map = true
+        vim.keymap.del('i', '<Tab>')
+    end
 }
