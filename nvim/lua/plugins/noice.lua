fcd8a441effc0edc04f518aa64e0fc05c41c44ab
Peter Panduro <email@peterpanduro.com>
2024-10-10 11:18:05 +0200

    Enable nvim-notify

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,19 +1,15 @@
 return {
     "folke/noice.nvim",
     dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        -- {
-        -- 	"rcarriga/nvim-notify",
-        -- 	config = function()
-        -- 		require("notify").setup({
-        -- 			background_colour = "#000000",
-        -- 		})
-        -- 	end,
-        -- },
+        {
+            "rcarriga/nvim-notify",
+            config = function()
+                require("notify").setup({
+                    background_colour = "#000000",
+                })
+            end,
+        },
     },
     event = "VeryLazy",
     opts = {
@@ -33,11 +29,11 @@ return {
         },
         -- you can enable a preset for easier configuration
         presets = {
-            bottom_search = true, -- use a classic bottom cmdline for search
-            command_palette = true, -- position the cmdline and popupmenu together
+            bottom_search = true,         -- use a classic bottom cmdline for search
+            command_palette = true,       -- position the cmdline and popupmenu together
             long_message_to_split = true, -- long messages will be sent to a split
-            inc_rename = false,  -- enables an input dialog for inc-rename.nvim
-            lsp_doc_border = false, -- add a border to hover docs and signature help
+            inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = false,       -- add a border to hover docs and signature help
         },
     },
 }
