9f48d13278f78019061b8dd3a4e09b4ad2e5d67e
Peter Panduro <email@peterpanduro.com>
2024-10-12 11:59:13 +0200

    Disable zellij.nvim

--- nvim/lua/plugins/zellij.lua
+++ nvim/lua/plugins/zellij.lua
@@ -1,7 +1,23 @@
-return {
-    "hiasr/vim-zellij-navigator.nvim",
-    event = "VeryLazy",
-    config = function()
-        require('vim-zellij-navigator').setup()
-    end
-}
+-- return {
+--     "hiasr/vim-zellij-navigator.nvim",
+--     event = "VeryLazy",
+--     config = function()
+--         require('vim-zellij-navigator').setup()
+--     end
+-- }
+
+-- return {
+--     "https://git.sr.ht/~swaits/zellij-nav.nvim",
+--     enabled = false,
+--     lazy = true,
+--     event = "VeryLazy",
+--     keys = {
+--         { "<a-h>", "<cmd>ZellijNavigateLeft<cr>",  { silent = true, desc = "navigate left" } },
+--         { "<a-j>", "<cmd>ZellijNavigateDown<cr>",  { silent = true, desc = "navigate down" } },
+--         { "<a-k>", "<cmd>ZellijNavigateUp<cr>",    { silent = true, desc = "navigate up" } },
+--         { "<a-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
+--     },
+--     opts = {},
+-- }
+
+return {}
